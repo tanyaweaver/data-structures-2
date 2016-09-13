2319a030efcc88d64f44167ef5293754a430962e
tanyaweaver <tanya83@uw.edu>
2016-09-13 16:46:55 -0700

    added docstrings

--- src/bst.py
+++ src/bst.py
@@ -8,12 +8,14 @@ class Node(object):
     """Define class for a node of a binary search tree."""
 
     def __init__(self, val):
+        """Create an instance of Node."""
         self.value = val
         self.left = None
         self.right = None
         self.depth = 1
 
     def find_depth(self):
+        """Find depth property of a node."""
         if self.left is not None:
             left_depth = self.left.depth
         else:
@@ -25,9 +27,8 @@ class Node(object):
         self.depth = 1 + max(left_depth, right_depth)
         return self.depth
 
-    def compare_self_to_a_node(self, new_node):
-        """Assume n1 and n2 are instances of Node."""
-        #import pdb; pdb.set_trace()
+    def _insert_node(self, new_node):
+        """Insert a new node. <new_node> must be an instance of Node."""
         parent = self
         visited_nodes = [self]
         while True:
@@ -54,8 +55,9 @@ class Node(object):
                     parent = parent.left
                     visited_nodes.append(parent)
 
-    def compare_nodes(self, n2):
-        """Assume n1 and n2 are instances of Node."""
+    def _compare_nodes(self, n2):
+        """Compare two nodes. Return False if values of 2 nodes are
+        not equal; return True otherwise."""
         parent = n2
         if self.value != parent.value:
             if self.value > parent.value:
@@ -63,13 +65,13 @@ class Node(object):
                     return False
                 else:
                     parent = parent.right
-                    return self.compare_nodes(parent)
+                    return self._compare_nodes(parent)
             elif self.value < parent.value:
                 if parent.left is None:
                     return False
                 else:
                     parent = parent.left
-                    return self.compare_nodes(parent)
+                    return self._compare_nodes(parent)
         else:
             return True
 
@@ -78,6 +80,9 @@ class Bst(object):
     """Define binary search tree class."""
 
     def __init__(self, iterable=None):
+        """
+        Create an instance of binary search tree.
+        """
         self.head = None
         self.counter = 0
 
@@ -90,7 +95,7 @@ class Bst(object):
         if self.head is None:
             self.head = new_node
         else:
-            self.head.compare_self_to_a_node(new_node)
+            self.head._insert_node(new_node)
         self.counter += 1
 
     def contains(self, value):
@@ -98,11 +103,10 @@ class Bst(object):
         Returns True if the value in the bst, False if not.
         """
         new_node = Node(value)
-        # import pdb; pdb.set_trace()
         if self.head is None:
             return False
         else:
-            if new_node.compare_nodes(self.head):
+            if new_node._compare_nodes(self.head):
                 return True
             else:
                 return False
@@ -119,3 +123,22 @@ class Bst(object):
         Return an integer representing the total levels in a tree.
         """
         return self.head.depth
+
+    def balance(self):
+        """
+        Return an integer that represents how well balanced the tree is:
+        - a positive value if the tree is higher on the left than the right;
+        - a negative value if the tree is higher on the right than the left;
+        - zero if the tree is ideally-balanced.
+        """
+        if self.head is None:
+            return 0
+        if self.head.left is not None:
+            left_depth = self.head.left.depth
+        else:
+            left_depth = 0
+        if self.head.right is not None:
+            right_depth = self.head.right.depth
+        else:
+            right_depth = 0
+        return left_depth - right_depth
--- src/test_bst.py
+++ src/test_bst.py
@@ -56,61 +56,61 @@ def test_node_init_depth(node):
 
 def test_compare_self_to_a_bigger_node1(node):
     """
-    Test that function compare_self_to_a_node(self, n)
+    Test that function _insert_node(self, n)
     assigns n.left = self.value if self.value is smaller than n.value.
     """
     node2 = Node(5)
-    node2.compare_self_to_a_node(node)
+    node2._insert_node(node)
     assert node2.left.value == 3
 
 
 def test_compare_self_to_a_bigger_node2(node):
     """
-    Test that function compare_self_to_a_node(self, n)
+    Test that function _insert_node(self, n)
     doesn't change n.right = None if self.value is smaller than n.value.
     """
     node2 = Node(5)
-    node2.compare_self_to_a_node(node)
+    node2._insert_node(node)
     assert node2.right is None
 
 
 def test_compare_self_to_a_smaller_node1(node):
     """
-    Test that function compare_self_to_a_node(self, n)
+    Test that function _insert_node(self, n)
     assigns n.right = self.value if self.value is bigger than n.value.
     """
     node1 = Node(7)
-    node.compare_self_to_a_node(node1)
+    node._insert_node(node1)
     assert node.right.value == 7
 
 
 def test_compare_self_to_a_smaller_node2(node):
     """
-    Test that function compare_self_to_a_node(self, n)
+    Test that function _insert_node(self, n)
     doesn't change n.left = None if self.value is bigger than n.value.
     """
     node1 = Node(7)
-    node.compare_self_to_a_node(node1)
+    node._insert_node(node1)
     assert node.left is None
 
 
 def test_compare_self_to_equal_value1(node):
     """
-    Test that function compare_self_to_a_node(self, n)
+    Test that function _insert_node(self, n)
     doesn't change n.left = None if self.value is equal to n.value.
     """
     node1 = Node(3)
-    node.compare_self_to_a_node(node1)
+    node._insert_node(node1)
     assert node.left is None
 
 
 def test_compare_self_to_equal_value2(node):
     """
-    Test that function compare_self_to_a_node(self, n)
+    Test that function _insert_node(self, n)
     doesn't change n.right = None if self.value is equal to n.value.
     """
     node1 = Node(3)
-    node.compare_self_to_a_node(node1)
+    node._insert_node(node1)
     assert node.right is None
 
 
@@ -189,30 +189,30 @@ def test_size_4(bst_3):
     assert bst_3.size() == 4
 
 def test_depth_node_insert1(node):
-    node.compare_self_to_a_node(Node(5))
+    node._insert_node(Node(5))
     assert node.depth == 2
 
 
 def test_depth_node_insert2(node):
-    node.compare_self_to_a_node(Node(5))
-    node.compare_self_to_a_node(Node(4))
+    node._insert_node(Node(5))
+    node._insert_node(Node(4))
     assert node.depth == 3
 
 
 def test_depth_node_insert4(node):
-    node.compare_self_to_a_node(Node(5))
-    node.compare_self_to_a_node(Node(4))
-    node.compare_self_to_a_node(Node(8))
-    node.compare_self_to_a_node(Node(7))
+    node._insert_node(Node(5))
+    node._insert_node(Node(4))
+    node._insert_node(Node(8))
+    node._insert_node(Node(7))
     assert node.depth == 4
 
 
 def test_depth_node_insert5(node):
-    node.compare_self_to_a_node(Node(5))
-    node.compare_self_to_a_node(Node(4))
-    node.compare_self_to_a_node(Node(8))
-    node.compare_self_to_a_node(Node(7))
-    node.compare_self_to_a_node(Node(6))
+    node._insert_node(Node(5))
+    node._insert_node(Node(4))
+    node._insert_node(Node(8))
+    node._insert_node(Node(7))
+    node._insert_node(Node(6))
     assert node.depth == 5
 
 
@@ -232,3 +232,46 @@ def test_depth_tree_insert3(bst_empty):
     bst_empty.insert(5)
     bst_empty.insert(6)
     assert bst_empty.depth() == 3
+
+
+def test_balance_empty(bst_empty):
+    assert bst_empty.balance() == 0
+
+
+def test_balance_2_neg(bst_empty):
+    bst_empty.insert(5)
+    bst_empty.insert(7)
+    assert bst_empty.balance() < 0
+
+
+def test_balance_2_pos(bst_empty):
+    bst_empty.insert(5)
+    bst_empty.insert(4)
+    assert bst_empty.balance() > 0
+
+
+def test_balance_3_balanced(bst_3):
+    assert bst_3.balance() == 0
+
+INSERT_BALANCE = [
+    [7, 4], [20, 13], [(4, 20)]
+]
+
+
+@pytest.mark.parametrize('val', INSERT_BALANCE[0])
+def test_balance_pos(val, bst_3):
+    bst_3.insert(val)
+    assert bst_3.balance() > 0
+
+
+@pytest.mark.parametrize('val', INSERT_BALANCE[1])
+def test_balance_neg(val, bst_3):
+    bst_3.insert(val)
+    assert bst_3.balance() < 0
+
+
+@pytest.mark.parametrize('val1, val2', INSERT_BALANCE[2])
+def test_balance_balanced(val1, val2, bst_3):
+    bst_3.insert(val1)
+    bst_3.insert(val2)
+    assert bst_3.balance() == 0
