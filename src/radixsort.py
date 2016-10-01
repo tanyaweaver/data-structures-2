3ac3a61e5ff81403077a5e3032f4a90572b7cade
clessor88 <clessor88@yahoo.com>
2016-10-01 13:56:05 -0700

    radixsort is working!

--- src/radixsort.py
+++ src/radixsort.py
@@ -1,6 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf -8 -*-
 
-from __future__ import unicode_literals
+from __future__ import unicode_literals, division
 from collections import deque
-from __future__ import division
+from math import log10
+
+
+def get_digit(num, position):
+    return num // 10 ** position % 10
+
+
+def put_nums_in_buckets(a_list, position):
+    q_list = []
+    for x in range(10):
+        queue_ = deque()
+        q_list.append(queue_)
+    # import pdb; pdb.set_trace()
+    for num in a_list:
+        digit = get_digit(num, position)
+        q_list[digit].append(num)
+    return q_list
+
+
+def find_num_of_iterations(a_list):
+    num_of_iterations = int(log10(max(a_list)) + 1)
+    return num_of_iterations
+
+
+def dequeue_queues_into_list(q_list):
+    a_list = []
+    for deque_ in q_list:
+        while len(deque_) != 0:
+            a_list.append(deque_.popleft())
+    return a_list
+
+
+def radixsort(a_list):
+    num_of_iterations = find_num_of_iterations(a_list)
+    pos = 0
+    while pos < num_of_iterations:
+        for num in a_list:
+            pending = put_nums_in_buckets(a_list, pos)
+            a_list = dequeue_queues_into_list(pending)
+            pos += 1
+    return a_list
--- src/test_radixsort.py
+++ src/test_radixsort.py
@@ -1,7 +1,87 @@
 #!/usr/bin/env python
 # -*- coding: utf -8 -*-
 
-from __future__ import unicode_literals
+from __future__ import division, unicode_literals
 from collections import deque
-from __future__ import division
-from radixsort import radixsort
+from radixsort import get_digit, put_nums_in_buckets, find_num_of_iterations, dequeue_queues_into_list, radixsort
+import pytest
+
+GET_DIGIT = [
+    (11, 0, 1),
+    (183, 2, 1),
+    (968438, 4, 6),
+    (9865987735468764, 11, 9),
+    (6354935, 6, 6),
+    (100, 1, 0),
+    (15, 7, 0)
+]
+
+
+BUCKETS = [
+    ([13, 8954, 4, 100, 2], 0,
+     [deque([100]), deque([]), deque([2]), deque([13]), deque([8954, 4]),
+      deque([]), deque([]), deque([]), deque([]), deque([])]),
+    ([1, 6544, 4687, 100, 76542], 3,
+     [deque([1, 100]), deque([]), deque([]), deque([]), deque([4687]),
+      deque([]), deque([6544, 76542]), deque([]), deque([]), deque([])])
+]
+
+
+ITERS = [
+    ([1, 6544, 4687, 100, 76542], 5),
+    ([13, 8954, 4, 100, 2], 4),
+    ([198563, 8954, 41, 0, 100, 2], 6),
+    ([13, 8, 4, 100, 2], 3),
+    ([1, 8, 4, 0, 2], 1),
+    ([1, 84, 4, 10, 2], 2),
+    ([13, 8954, 6198468734, 100, 2], 10)
+]
+
+
+DEQUES = [
+    ([deque([100]), deque([]), deque([2]), deque([13]), deque([8954, 4]),
+      deque([]), deque([]), deque([]), deque([]), deque([])],
+     [100, 2, 13, 8954, 4]),
+    ([deque([1, 100]), deque([]), deque([]), deque([]), deque([4687]),
+      deque([]), deque([6544, 76542]), deque([]), deque([]), deque([])],
+     [1, 100, 4687, 6544, 76542])
+]
+
+RADIX = [
+    ([48, 1, 107], [1, 48, 107]),
+    ([100, 2, 13, 8954, 4], [2, 4, 13, 100, 8954]),
+    ([1, 6544, 4687, 100, 76542], [1, 100, 4687, 6544, 76542]),
+    ([198563, 8954, 41, 0, 100, 2, 0], [0, 0, 2, 41, 100, 8954, 198563])
+]
+
+
+@pytest.mark.parametrize('num, position, digit', GET_DIGIT)
+def test_get_digit(num, position, digit):
+    assert get_digit(num, position) == digit
+
+
+@pytest.mark.parametrize('a_list, position, q_list', BUCKETS)
+def test_put_nums_in_buckets(a_list, position, q_list):
+    assert put_nums_in_buckets(a_list, position) == q_list
+
+
+@pytest.mark.parametrize('a_list, result', ITERS)
+def test_find_num_of_iterations(a_list, result):
+    assert find_num_of_iterations(a_list) == result
+
+
+@pytest.mark.parametrize('q_list, result', DEQUES)
+def test_dequeue_queues_into_list(q_list, result):
+    assert dequeue_queues_into_list(q_list) == result
+
+
+@pytest.mark.parametrize('a_list, result', RADIX)
+def test_radixsort(a_list, result):
+    assert radixsort(a_list) == result
+
+
+def test_radixsort_is_stable():
+    a_list = [15, 15, 15, 15, 15]
+    result = radixsort(a_list)
+    for i in range(len(a_list)):
+        assert a_list[i] is result[i]
