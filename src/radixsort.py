270e977771d68126775b51ed10381c5c803adb54
clessor88 <clessor88@yahoo.com>
2016-10-01 14:50:15 -0700

    added if name == main block with times for each list

--- src/radixsort.py
+++ src/radixsort.py
@@ -45,3 +45,38 @@ def radixsort(a_list):
                 a_list = _dequeue_queues_into_list(pending)
                 pos += 1
     return a_list
+
+
+if __name__ == '__main__':
+    import time
+    from random import sample
+    LISTS = (
+        [1],
+        [1, 2, 6, 3, 4, 5, 0, 9, 8],
+        [6851] * 100,
+        sample(range(10000), 100),
+        list(range(100)),
+        [11] * 99 + [100000000]
+    )
+    times = []
+    for list_ in LISTS:
+        start_time = time.time()
+        radixsort(list_)
+        end_time = time.time()
+        t = (end_time - start_time) * 1000
+        times.append(t)
+    print('Radix sort is a non-comparative integer sorting algorithm')
+    print('that sorts data with integer keys by grouping keys')
+    print('by the individual digits which share the same significant')
+    print('position and value. ')
+    print('Radix sort best performs for short lists of small numbers.')
+    print('Average run time for:')
+    print('1) list with 1 small item: {} ms.'.format(times[0]))
+    print('2) list with a few small items: {} ms.'.format(times[1]))
+    print('3) list with 100 identical items: {} ms.'.format(times[2]))
+    print('4) unsorted list with 100 items (range 0 - 10000):')
+    print('{} ms.'.format(times[3]))
+    print('5) sorted list with 100 items (range 0 - 10000):')
+    print('{} ms.'.format(times[4]))
+    print('6) unsorted list with 100 items (99 of 11\'s, and 100000000):')
+    print('{} ms.'.format(times[5]))
