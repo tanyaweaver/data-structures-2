c9ec84874d1d3cab721be3b493a81d21b5d3e6ec
tanyaweaver <tanya83@uw.edu>
2016-10-01 18:41:14 -0700

    all tests are passing

--- src/quicksort.py
+++ src/quicksort.py
@@ -10,11 +10,10 @@ def _sort_relative_to_pivot(a_list, pivot_pos):
     Sort items in the list relative to the pivot.
     Return position of the pivot after the sort completed.
     """
-    #import pdb; pdb.set_trace()
     pivot = a_list[pivot_pos]
+    i = 0
+    x = len(a_list) - 1
     while True:
-        i = 0
-        x = len(a_list) - 1
         while a_list[i] < pivot or (a_list[i] == pivot and i != pivot_pos):
             i += 1
         while a_list[x] > pivot:
@@ -30,7 +29,6 @@ def _sort_relative_to_pivot(a_list, pivot_pos):
 
 def quicksort(a_list):
     """Return sorted list using quicksort algorithm."""
-    #import pdb; pdb.set_trace()
     if len(a_list) <= 1:
         return a_list
     pivot_pos = choice(range(len(a_list)))
